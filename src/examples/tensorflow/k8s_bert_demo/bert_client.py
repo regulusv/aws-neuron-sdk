b40a40dca21ee59899ff9525b440e0f05fd819d9
Gilbert <artemisgray@gmail.com>
2020-05-20 23:26:57 -0700

    Update bert_client.py
    for k8s test job

--- src/examples/tensorflow/k8s_bert_demo/bert_client.py
+++ src/examples/tensorflow/k8s_bert_demo/bert_client.py
@@ -6,7 +6,8 @@ from tensorflow_serving.apis import prediction_service_pb2_grpc
 import time
 
 if __name__ == '__main__':
-    channel = grpc.insecure_channel('localhost:9000')
+    # channel = grpc.insecure_channel('localhost:9000')
+    channel = grpc.insecure_channel('10.100.55.91:9000')
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
     request = predict_pb2.PredictRequest()
     request.model_spec.name = 'bert_mrpc_hc_gelus_b4_l24_0926_02'
