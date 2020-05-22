ceb3d74300436ba46d9fed8403704a8d464651fd
Gilbert <artemisgray@gmail.com>
2020-05-22 03:03:55 -0700

    Update bert_client.py

--- src/examples/tensorflow/k8s_bert_demo/bert_client.py
+++ src/examples/tensorflow/k8s_bert_demo/bert_client.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 if __name__ == '__main__':
-    channel = grpc.insecure_channel('localhost:9000')
+    channel = grpc.insecure_channel('svc/eks-neuron-test-bert-service:9000')
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
     request = predict_pb2.PredictRequest()
     request.model_spec.name = 'bert_mrpc_hc_gelus_b4_l24_0926_02'
